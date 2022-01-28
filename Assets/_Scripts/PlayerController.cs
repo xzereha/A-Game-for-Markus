842529ae8ebf59fe857401e400ba147292f007b3
Oliver Öhrström <XZereha@live.se>
2022-01-28 22:37:10 +0100

    Acceleration

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -6,29 +6,44 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class PlayerController : MonoBehaviour, PlayerInput.IGameplayActions
 {
+    [Tooltip("The player rigidbody")]
     [SerializeField] private Rigidbody2D m_RigidBody;
+
+    [Tooltip("The player collider")]
     [SerializeField] private Collider2D m_Collider;
+
+    [Tooltip("Box collider for detecting if the player is grounded or not")]
     [SerializeField] private BoxCollider2D m_GroundCheckCollider;
+
+    [Tooltip("What collision layers should count as ground")]
     [SerializeField] private LayerMask m_CollisionLayer;
 
     [Header("Jumping")]
     [Tooltip("How high is the peak of the jump")]
     [SerializeField] private float m_JumpHeight = 2.0f;
+
     [Tooltip("How long does it take to reach the peak of the jump")]
     [SerializeField] private float m_TimeToApex = 1.0f;
+    
     [Tooltip("How much what is the modifier for the falling speed")]
     [SerializeField] private float m_FallingModifier = 1.0f;
 
     [Header("Movement")]
+    [Tooltip("Player movement speed in units per second")]
     [SerializeField] private float m_MovementSpeed = 2.0f;
 
-    private PlayerInput m_Controls;
-    private float m_JumpVelocity;
-    private float m_Gravity;
-    private float m_YVelocity;
-    private float m_InputDirection;
-    private bool m_Grounded;
-    private bool m_Jumping;
+    [Tooltip("Time to reach maximum movement speed")]
+    [SerializeField] private float m_AccelerationTime = 0.1f;
+
+    private PlayerInput m_Controls; //!< The object for the input hangler
+    private float m_JumpVelocity; //!< The velocity constant for the jump
+    private float m_Gravity; //!< The gravity constant for the jump
+
+    private float m_InputDirection; //!< The direction the player is moving
+    private bool m_Grounded; //!< If the player is currently touching the ground
+    private bool m_Jumping; //!< If the player is still going upwards
+    private float m_XVelocity; //!< Current X velocity of the player
+    private float m_YVelocity; //!< Current Y velocity of the player
 
     private void Awake() 
     {
@@ -53,11 +68,21 @@ public class PlayerController : MonoBehaviour, PlayerInput.IGameplayActions
 
     private void FixedUpdate() 
     {
+        // Ground Check
         RaycastHit2D hit = Physics2D.BoxCast(m_GroundCheckCollider.transform.position + (Vector3)m_GroundCheckCollider.offset, m_GroundCheckCollider.size, 0, Vector2.down, 0.0f, m_CollisionLayer);
         m_Grounded = hit.collider? true : false;
+
+        // Jump handling
         m_Jumping = m_Jumping && m_YVelocity > 0.0f;
         m_YVelocity = m_Grounded? Mathf.Max(m_YVelocity, 0) : m_YVelocity + m_Gravity * (m_Jumping? 1.0f : m_FallingModifier)  * Time.deltaTime;
-        transform.Translate(new Vector3(m_InputDirection * m_MovementSpeed * Time.deltaTime, m_YVelocity * Time.deltaTime, 0));
+
+        // Movement handling
+        float acceleration = m_MovementSpeed / m_AccelerationTime;
+        m_XVelocity += acceleration * m_InputDirection * Time.deltaTime;
+        m_XVelocity = Mathf.Clamp(m_XVelocity, -m_MovementSpeed, m_MovementSpeed);
+
+        // Final movement
+        transform.Translate(new Vector3(m_XVelocity * Time.deltaTime, m_YVelocity * Time.deltaTime, 0));
     }
 
 #region Helpers
@@ -73,6 +98,7 @@ public class PlayerController : MonoBehaviour, PlayerInput.IGameplayActions
     {
         float dir = ctx.ReadValue<float>();
         m_InputDirection = dir;
+        m_XVelocity = dir == 0? 0 : m_XVelocity;
     }
 
     public void OnJump(InputAction.CallbackContext ctx)
@@ -94,6 +120,7 @@ public class PlayerController : MonoBehaviour, PlayerInput.IGameplayActions
     {
         // Update jump height incase the variable was changed
         CalulateJumpValues();
+        m_AccelerationTime = Mathf.Max(m_AccelerationTime, 0.001f);
     }
 
     private void Reset() 
